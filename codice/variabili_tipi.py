1c5a6a5183746fc8d9ce6d13b9614a4540342e22
Mauro Bogliaccino <mauro.bogliaccino@gmail.com>
2025-12-23 12:54:23 +0100

    variabili e tipi

---
+++ codice/variabili_tipi.py
@@ -0,0 +1,17 @@
+# Dichiarazione di variabili
+nome = "Mario"
+eta = 25
+altezza = 1.75
+is_studente = True
+
+print(type(eta))
+
+
+# Stampa delle variabili
+print("Nome:", nome)
+print("Età:", eta)
+print("Altezza:", altezza)
+print("È studente:", is_studente)
+
+# cast forzatura del tipo
+print(str(eta)  + str(eta))
