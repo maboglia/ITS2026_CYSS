1a81044a509804d50d57f4c9e72b3b9bc07c2423
Mauro Bogliaccino <mauro.bogliaccino@gmail.com>
2026-01-21 12:22:50 +0100

    codice lez 02 - candidati e risultati

--- insert into candidati (cognome) values (.sql
+++
@@ -1,7 +0,0 @@
-insert into candidati (cognome) values ('Alfonso'); 
-insert into candidati (cognome) values ('Finocchielli'); 
-insert into candidati (cognome) values ('Jmai'); 
-insert into candidati (cognome) values ('Moccia'); 
-insert into candidati (cognome) values ('Passiatore'); 
-insert into candidati (cognome) values ('Regaldo'); 
-insert into candidati (cognome) values ('Romanu');
