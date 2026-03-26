2ef94e35eccdefea16fbb00b75aaec532bf6144c
maboglia <mauro.bogliaccino@gmail.com>
2026-03-26 11:46:19 +0100

    magazzino v 0.3

--- codice/L10/prodotti_repo.py
+++ codice/L10/prodotti_repo.py
@@ -2,24 +2,30 @@ import connessione
 
 class ProdottiRepo:
     def __init__(self):
-        self.conn = connessione.Connessione()
+        self.conn = connessione.connect_to_database()
+        self.cursor = self.conn.cursor()
 
     def get_prodotti(self):
-        query = "SELECT * FROM prodotti"
-        return self.conn.execute_query(query)
+        query = "SELECT * FROM Prodotti"
+        self.cursor.execute(query)
+        return self.cursor.fetchall()
 
     def get_prodotto_by_id(self, prodotto_id):
-        query = "SELECT * FROM prodotti WHERE id = %s"
-        return self.conn.execute_query(query, (prodotto_id,))
+        query = "SELECT * FROM Prodotti WHERE id = %s"
+        self.cursor.execute(query, (prodotto_id,))
+        return self.cursor.fetchone()
 
     def add_prodotto(self, nome, prezzo):
-        query = "INSERT INTO prodotti (nome, prezzo) VALUES (%s, %s)"
-        self.conn.execute_query(query, (nome, prezzo))
+        query = "INSERT INTO Prodotti (nome, prezzo) VALUES (%s, %s)"
+        self.cursor.execute(query, (nome, prezzo))
+        self.conn.commit()
 
     def update_prodotto(self, prodotto_id, nome, prezzo):
-        query = "UPDATE prodotti SET nome = %s, prezzo = %s WHERE id = %s"
-        self.conn.execute_query(query, (nome, prezzo, prodotto_id))
+        query = "UPDATE Prodotti SET nome = %s, prezzo = %s WHERE id = %s"
+        self.cursor.execute(query, (nome, prezzo, prodotto_id))
+        self.conn.commit()
 
     def delete_prodotto(self, prodotto_id):
-        query = "DELETE FROM prodotti WHERE id = %s"
-        self.conn.execute_query(query, (prodotto_id,))
+        query = "DELETE FROM Prodotti WHERE id = %s"
+        self.cursor.execute(query, (prodotto_id,))
+        self.conn.commit()
